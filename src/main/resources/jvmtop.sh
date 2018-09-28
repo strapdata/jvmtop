6639255b0d341f45d0f0c3a593d28b73f35ff1d7
Vincent Royer <vroyer@strapdata.com>
2018-09-28 16:48:05 +0200

    Check JAVA_HOME

--- src/main/resources/jvmtop.sh
+++ src/main/resources/jvmtop.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ -z $JAVA_HOME ]; then
+   echo "Please set JAVA_HOME"
+   exit 1
+fi
 exec java -Xmx1G -Djava.library.path="$JAVA_HOME/jre/bin" -cp "$0:$JAVA_HOME/lib/tools.jar" com.jvmtop.JvmTop "$@"
 
